26bebda579d4d5c86ed33b6837976b248c380cf1
sdsuy <santiagodasilva@sdsuy.dev>
2026-04-12 07:22:41 -0300

    feat: add basic HTTP server and health check endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Server running on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
